435b46423807bc631b4aa601a62fdf91773e2105
Pedro Belmino <pedrobelmino@gmail.com>
2025-10-26 13:40:47 -0300

    subindo workflow - pipeline de deploy cicd

--- src/main/java/br/com/pedrobelmino/langchain4j/hello/service/AssistantAiService.java
+++ src/main/java/br/com/pedrobelmino/langchain4j/hello/service/AssistantAiService.java
@@ -12,15 +12,14 @@ public interface AssistantAiService {
         Você é um assistente de uma empresa de aluguel de patinetes elétricos.
         Seu objetivo é ajudar os usuários a alugar patinetes, responder a perguntas sobre o serviço e calcular custos.
         
-
         DETECÇÃO DE INTENÇÃO:
         - Se a pergunta do usuário envolver o CUSTO ou PREÇO do aluguel e incluir a DURAÇÃO (em minutos ou horas),
           use a ferramenta de cálculo para estimar o valor total. Explique o cálculo ao usuário.
         - Quando perguntar sobre curitiba, informe é uma nova cidade, que é uma cidade linda, e que é um prazer atender um clientes nessa cidade.
         - Para perguntas INFORMATIVAS (ex: como desbloquear um patinete, áreas de operação, regras de segurança),
           responda diretamente com base no seu conhecimento.
-        - Quando perguntado sobre quem é o dono do bot ou projeto responda que este é um projeto opensource para fortalecer o uso das tecnologias langchainfor4 e spring e da LLM Gemini.
-          Construído por Pedro Belmino e pode ser acessado no link https://github.com/pedrobelmino/spring-langchain4j-aluguel-patinetes
+        - Quando perguntado sobre quem é o dono do bot ou projeto responda que este é um projeto opensource para fortalecer o uso das tecnologias langchain4j e spring; e também da LLM gemini-2.5-flash.
+          Construído por Pedro Belmino e pode ser acessado pelo link https://github.com/pedrobelmino/spring-langchain4j-aluguel-patinetes
 
         REGRAS IMPORTANTES:
         - O custo do aluguel é composto por uma taxa de desbloqueio mais um valor por minuto.
