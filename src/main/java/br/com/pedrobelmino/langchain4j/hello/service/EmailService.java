38989bad12158a36ca319775da08499d6a036163
Pedro Belmino <pedrobelmino@gmail.com>
2026-01-04 20:53:36 -0300

    substituindo envio de e-mails com e-mail jet

--- src/main/java/br/com/pedrobelmino/langchain4j/hello/service/EmailService.java
+++ src/main/java/br/com/pedrobelmino/langchain4j/hello/service/EmailService.java
@@ -1,44 +1,50 @@
 package br.com.pedrobelmino.langchain4j.hello.service;
 
-import com.sendgrid.Method;
-import com.sendgrid.Request;
-import com.sendgrid.Response;
-import com.sendgrid.SendGrid;
-import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Content;
-import com.sendgrid.helpers.mail.objects.Email;
+import com.mailjet.client.ClientOptions;
+import com.mailjet.client.MailjetClient;
+import com.mailjet.client.MailjetRequest;
+import com.mailjet.client.MailjetResponse;
+import com.mailjet.client.errors.MailjetException;
+import com.mailjet.client.resource.Emailv31;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
 
 @Service
 public class EmailService {
 
-    @Value("${spring.sendgrid.api-key}")
-    private String sendGridApiKey;
+    @Value("${mailjet.api-key}")
+    private String mailjetApiKey;
+
+    @Value("${mailjet.secret-key}")
+    private String mailjetSecretKey;
 
     @Value("${EMAIL_USER}")
     private String fromEmail;
 
-    public void sendEmail(String to, String subject, String body) throws IOException {
-        Email from = new Email(fromEmail);
-        Email toEmail = new Email(to);
-        Content content = new Content("text/plain", body);
-        Mail mail = new Mail(from, subject, toEmail, content);
-
-        SendGrid sg = new SendGrid(sendGridApiKey);
-        Request request = new Request();
-        try {
-            request.setMethod(Method.POST);
-            request.setEndpoint("mail/send");
-            request.setBody(mail.build());
-            Response response = sg.api(request);
-            System.out.println(response.getStatusCode());
-            System.out.println(response.getBody());
-            System.out.println(response.getHeaders());
-        } catch (IOException ex) {
-            throw ex;
-        }
+    public void sendEmail(String to, String subject, String body) throws MailjetException {
+        MailjetClient client;
+        MailjetRequest request;
+        MailjetResponse response;
+        client = new MailjetClient(
+                ClientOptions.builder()
+                        .apiKey(mailjetApiKey)
+                        .apiSecretKey(mailjetSecretKey)
+                        .build());
+        request = new MailjetRequest(Emailv31.resource)
+                .property(Emailv31.MESSAGES, new JSONArray()
+                        .put(new JSONObject()
+                                .put(Emailv31.Message.FROM, new JSONObject()
+                                        .put("Email", fromEmail))
+                                .put(Emailv31.Message.TO, new JSONArray()
+                                        .put(new JSONObject()
+                                                .put("Email", to)))
+                                .put(Emailv31.Message.SUBJECT, subject)
+                                .put(Emailv31.Message.TEXTPART, body)
+                                .put(Emailv31.Message.HTMLPART, "<h3>" + body + "</h3>")));
+        response = client.post(request);
+        System.out.println(response.getStatus());
+        System.out.println(response.getData());
     }
 }
